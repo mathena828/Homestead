02d3ff407725a836b8afb33e3a87b3f04d9a8d22
mathena828 <mathena828@gmail.com>
2020-02-04 01:09:39 +0800

    Bug Fixes

--- app/models/property.rb
+++ app/models/property.rb
@@ -4,6 +4,6 @@ class Property < ApplicationRecord
 	validates :property_address, :monthly_rent, presence: true
 	validates :property_address, uniqueness: {case_sensitive: false}
 	validates :monthly_rent, numericality: {greater_than_or_equal_to: 0}
-	has_attached_file :image, styles: { medium: "300x300#" }
+	has_attached_file :image, styles: { medium: "400x300#" }
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
