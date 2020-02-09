8bbb140cac71a744f825f2048cf170be4ccd6aaa
mathena828 <mathena828@gmail.com>
2020-02-09 18:19:01 +0800

    Heroku

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -2,8 +2,14 @@ class ApplicantsController < ApplicationController
 	before_action :find_property
 	def create
 		@applicant = @property.applicants.create(applicant_params)
-		redirect_to property_path(@property)
-		
+		  if @applicant.save
+		    redirect_to property_path(@property), notice: 'Successfully Submitted New Application.'
+		  else
+		  	flash[:error] = "The application could not be submitted. Please verify that none of the input 
+		  	fields are blank. The social security number must be in the xxx-xx-xxxx format and the phone 
+		  	number must be in the xxx-xxx-xxxx format."
+		    redirect_to property_path(@property)
+		  end
 	end
 	def edit
  	   @applicant = @property.applicants.find(params[:id])
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,7 +1,7 @@
 class Property < ApplicationRecord
 	belongs_to :landlord
 	has_many :applicants, dependent: :destroy
-	validates :property_address, :monthly_rent, presence: true
+	validates :property_address, :monthly_rent, :image, presence: true
 	validates :property_address, uniqueness: {case_sensitive: false}
 	validates :monthly_rent, numericality: {greater_than_or_equal_to: 0}
 	has_attached_file :image, styles: { medium: "400x300#" }
